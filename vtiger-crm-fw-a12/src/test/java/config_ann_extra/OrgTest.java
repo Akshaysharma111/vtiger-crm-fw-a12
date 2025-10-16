22b36c3f501aabf1417297164eca8a7beb0d64a1
akshay sharma <akshay sharma@AKSHAY-PC>
2025-10-16 11:36:53 +0530

    working with assertion

--- vtiger-crm-fw-a12/src/test/java/config_ann_extra/OrgTest.java
+++ vtiger-crm-fw-a12/src/test/java/config_ann_extra/OrgTest.java
@@ -2,6 +2,8 @@ package config_ann_extra;
 
 import org.testng.annotations.Test;
 
+import base_utility.BaseClass;
+
 public class OrgTest extends BaseClass {
 	@Test
 	public void createOppTest() {
--- vtiger-crm-fw-a12/src/test/java/organization/CreateOrganization.java
+++ vtiger-crm-fw-a12/src/test/java/organization/CreateOrganization.java
@@ -18,97 +18,52 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class CreateOrganization {
-	public static void main(String[] args) throws InterruptedException, EncryptedDocumentException, IOException {
-		
-//		1.	Get the Java Representation Object of the physical file
-		FileInputStream fis = new FileInputStream("./src/test/resources/CommanData.properties");
+import base_utility.BaseClass;
+import generic_utility.FileUtility;
+import generic_utility.JavaUtility;
+import object_repository.HomePage;
+import object_repository.OrgPage;
+import object_repository.VerifyOrgPage;
 
-//		2.	Use the load() of Properties class to load all the keys
-		Properties pObj = new Properties();
-		pObj.load(fis);
+public class CreateOrganization extends BaseClass{
+	
 		
-//		3.	Use the getProperty(Key) and pass the key and get the value in String format
-		String BROWSER = pObj.getProperty("bro");
-		String URL = pObj.getProperty("url");
-		String USERNAME = pObj.getProperty("un");
-		String PASSWORD = pObj.getProperty("pwd");
 		
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		driver.get(URL);
-		
-//		Login
-		WebElement username = driver.findElement(By.name("user_name"));
-		WebElement password = driver.findElement(By.name("user_password"));
-		username.sendKeys(USERNAME);
-		password.sendKeys(PASSWORD);
-		driver.findElement(By.id("submitButton")).click();
+@Test
+public void createOrgTest() throws EncryptedDocumentException, IOException, InterruptedException {
 
-		
-		
-//		Create Organization
-		driver.findElement(By.linkText("Organizations")).click();
-		driver.findElement(By.cssSelector("img[title='Create Organization...']")).click();
-		//org name
-		FileInputStream fiss = new FileInputStream("./src/test/resources/testScriptData.xlsx");
-		Workbook wb = WorkbookFactory.create(fiss);
-		Sheet sh = wb.getSheet("a12");
-		//Row rw = sh.createRow(11);
-		//Cell cell = rw.createCell(0);
-		//cell.setCellValue("googlechrome_");
-		//save the changes
-		//FileOutputStream fos = new FileOutputStream("./src/test/resources/TestScriptDataToSet.xlsx");
-		//wb.write(fos);
-		
-		Row row = sh.getRow(5);
-		
-		Cell cell1 = row.getCell(0);
-		
-		String orgName = cell1.getStringCellValue() + (int)(Math.random()*9999);;
-		
-		System.out.println(orgName);
-		
-		wb.close();
-		//String orgName = "qspiders_" + (int)(Math.random()*9999);
-		WebElement orgField = driver.findElement(By.name("accountname"));
-		orgField.sendKeys(orgName);
-		   //no of employee
-				String empno = ""+ (int)(Math.random()*9999);
-				 WebElement employe= driver.findElement(By.id("employees"));
-				 employe.sendKeys(empno);
-			
-				 //industry
-				 WebElement industryDropdown = driver.findElement(By.name("industry"));
-			        Select select = new Select(industryDropdown);
-			        select.selectByVisibleText("Banking");
-			        String selectedIndustry = select.getFirstSelectedOption().getText();
+//	Create Organization
+	HomePage hp = new HomePage(driver);
+	hp.getOrgLink().click();
 
-			   	 driver.findElement(By.xpath("//input[@value='  Save  ']")).click();
-//				Verification
-				
-				String actOrgName = driver.findElement(By.id("dtlview_Organization Name")).getText();
-				String empcount = driver.findElement(By.id("mouseArea_Employees")).getText();
-				String selectind = driver.findElement(By.id("mouseArea_Industry")).getText();
-				if (actOrgName.trim().equals(orgName ) &&  empcount.trim().equals(empno) && selectind.trim().equals(selectedIndustry)) {
-					System.out.println("Organization created successfully!!!");
-				}else {
-					System.out.println("Organization could not be created successfully!!!");
-				}
-				
-//				Logout
-				WebElement profile = driver.findElement(By.cssSelector("img[src='themes/softed/images/user.PNG']"));
-				
-				Actions act = new Actions(driver);
-				act.moveToElement(profile).build().perform();
-				
-				driver.findElement(By.linkText("Sign Out")).click();
-				
-				Thread.sleep(3000);
-				driver.quit();
-		
-	}
+	OrgPage op = new OrgPage(driver);
+
+	op.getOrgPlusIcon().click();
+
+	FileUtility fUtil = new FileUtility();
+	String orgName = fUtil.getStringDataFromExcelFile("org", 3, 0) + JavaUtility.generateRandomNum();
+
+	WebElement orgField = op.getOrgField();
+	orgField.sendKeys(orgName);
+
+//	Save 
+	op.getSaveBtn().click();
+
+//	Verification
+	VerifyOrgPage vop = new VerifyOrgPage(driver);
+	String actOrgName = vop.getActOrgName().getText();
+
+	Assert.assertEquals(actOrgName, orgName);
+	
+//	if (actOrgName.equals(orgName + "abc")) {
+//		System.out.println("Created Organization successfully!!!!");
+//	} else {
+//		System.out.println("Failed....");
+//	}
+
+}
 
 }
